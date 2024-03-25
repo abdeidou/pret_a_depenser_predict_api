b2db7d373955d96fb230c480ed86c8f34d62769f
aziz <abdelaaziz.idoumohmed.2023@gmail.com>
2024-03-25 11:38:21 +0100

    test simple flask service

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask, request
+import pandas as pd
+import pickle
+from waitress import serve
+
+# Créer une instance de l'application Flask
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return "Hello"
+
+# Lancer le processus flask
+if __name__ == '__main__':
+    serve(app, host="0.0.0.0", port=8080)
