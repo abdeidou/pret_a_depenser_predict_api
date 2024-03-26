165113a2fa66c4962c57896c215deaff9bf931bc
abdeidou <160952495+abdeidou@users.noreply.github.com>
2024-03-26 10:38:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return "Hello YouTube!! I hope you liked it"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
-# test g cloud 2
