b9e295acf0d78a04987a20cd02109e9fdc805395
aziz <abdelaaziz.idoumohmed.2023@gmail.com>
2024-03-27 11:41:09 +0100

    test git hub action to deploy 37

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello YouTube!! I hope you liked it"
 
+@app.route('/double', methods=['GET'])
+def multiple():
+    number = request.args.get("number")
+    result = number * 2
+    return str(result)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
