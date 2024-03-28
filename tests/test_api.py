c7ba2a2f2c40883f111c230d889517e5e7b5f176
aziz <abdelaaziz.idoumohmed.2023@gmail.com>
2024-03-28 13:58:39 +0100

    modif test_api undo case redirect to deploy

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,57 +3,48 @@ import json
 from main import customers_data, customers_data_ohe, lgbm
 from main import app
 
-# Fixtures for configuration
+# Les fixtures pour la configuration
 
-# Fixture providing a Flask test client
+# Fixture fournissant un client de test Flask
 @pytest.fixture()
 def client():
     with app.test_client() as client:
         return client
-
-# Fixture providing an example customer_id for tests
+# Fixture fournissant un customer_id d'exemple pour les tests
 @pytest.fixture()
 def customer_id():
     return 100038
 
-# Fixture providing expected JSON data for a customer_id
+# Fixture fournissant des données JSON attendues pour un customer_id
 @pytest.fixture()
 def expected_customer_data(customer_id):
     return customers_data[customers_data['SK_ID_CURR'] == str(customer_id)].to_json()
 
-# Fixture providing the expected prediction result for a customer_id
+# Fixture fournissant le résultat de prédiction attendu pour un customer_id
 @pytest.fixture()
 def expected_customer_predict(customer_id):
     customer_row = customers_data[customers_data['SK_ID_CURR'] == str(customer_id)]
     customer_row_ohe = customers_data_ohe.iloc[customer_row.index].drop(columns=['SK_ID_CURR'], axis=1)
     return lgbm.predict_proba(customer_row_ohe).tolist()
 
-# Test functions
+# Les fonctions de test
 
-# Test function for customer_data
+# Fonction test de customer_data
 def test_customer_data_api(client, customer_id, expected_customer_data):
-    # Make a request to the API
-    with client.get(f"/customer_data", query_string={"customer_id": customer_id}, allow_redirects=False) as response:
-        # Follow the redirect if present
-        if response.status_code == 308:
-            location = response.headers["Location"]
-            response = client.get(location)
-        # Verify the response status
+    # Faire une requête à l'API
+    with client.get(f"/customer_data", query_string={"customer_id": customer_id}) as response:
+        # Vérifier le statut de la réponse
         assert response.status_code == 200
-        # Verify the response
-        response_data = json.loads(response.data)
+        # Vérifier la réponse
+        response_data = json.loads(response.text)
         assert response_data['customer_data'] == expected_customer_data
 
-# Test function for predict
+# Fonction test de predict
 def test_predict_api(client, customer_id, expected_customer_predict):
-    # Make a request to the API
-    with client.get(f"/predict", query_string={"customer_id": customer_id}, allow_redirects=False) as response:
-        # Follow the redirect if present
-        if response.status_code == 308:
-            location = response.headers["Location"]
-            response = client.get(location)
-        # Verify the response status
+    # Faire une requête à l'API
+    with client.get(f"/predict", query_string={"customer_id": customer_id}) as response:
+        # Vérifier le statut de la réponse
         assert response.status_code == 200
-        # Verify the response
-        response_data = json.loads(response.data)
+        # Vérifier la réponse
+        response_data = json.loads(response.text)
         assert response_data['customer_predict'] == expected_customer_predict
