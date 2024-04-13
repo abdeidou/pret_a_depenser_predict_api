2acc0881906e4cb5504ac8955c073a4de9169493
aziz <abdelaaziz.idoumohmed.2023@gmail.com>
2024-04-13 19:00:10 +0200

    modif explain_global to deploy

--- main.py
+++ main.py
@@ -75,18 +75,27 @@ def explain_local():
 
 @app.route('/explain_global')
 def explain_global():
-    # Créer le graphique SHAP beeswarm
-    #fig = shap.plots.beeswarm(shap_values)
-    fig = shap.summary_plot(shap_values, X)
-    # Enregistrer le graphique dans un buffer mémoire
-    buf = io.BytesIO()
-    fig.savefig(buf, format='png')
-    buf.seek(0)
-    # Convertir le graphique en base64
-    graph_data = base64.b64encode(buf.read()).decode('utf-8')
-    # Créer la réponse JSON avec les données du graphique
-    response = {'shap_plot': graph_data}
-    return jsonify(response)
+    try:
+        # Créer le graphique SHAP beeswarm
+        fig = shap.plots.beeswarm(shap_values)
+
+        if fig:
+            # Enregistrer le graphique dans un buffer mémoire
+            buf = io.BytesIO()
+            fig.savefig(buf, format='png')
+            buf.seek(0)
+
+            # Convertir le graphique en base64
+            graph_data = base64.b64encode(buf.read()).decode('utf-8')
+
+            # Créer la réponse JSON avec les données du graphique
+            response = {'shap_plot': graph_data}
+
+            return jsonify(response)
+        else:
+            return jsonify({'error': 'Impossible de générer le graphique SHAP'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 @app.route('/threshold')
