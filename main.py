ee5f6591f1679d2a4f08d41daf5b2918c2921251
aziz <abdelaaziz.idoumohmed.2023@gmail.com>
2024-04-19 10:05:45 +0200

    png to deploy

--- main.py
+++ main.py
@@ -77,11 +77,11 @@ def explain_local():
     shap.waterfall_plot(explanation[int(customer_index.values[0])], show=False)
     # Save plot to BytesIO
     buffer = io.BytesIO()
-    plt.savefig(buffer, dpi=250, format="pdf")
+    plt.savefig(buffer, dpi=250, format="png")
     plt.close()
     # Rewind BytesIO
     buffer.seek(0)
-    return send_file(buffer, mimetype='image/pdf')
+    return send_file(buffer, mimetype='image/png')
 
 
 @cache.cached(timeout=300, key_prefix='explain_global')
